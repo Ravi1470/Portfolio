0cc5234256b56b816a6c376fd8eefaac13059e02
Ravi1470 <vk8580318@gmail.com>
2025-02-24 14:11:24 +0530

    portfolio completed

---
+++ src/Components/ContactForm.jsx
@@ -0,0 +1,96 @@
+import React, { useState } from "react";
+import emailjs from "emailjs-com";
+const ContactForm = () => {
+  // const to_name = "Sri Haripriyan";
+  const [from_name, setFromName] = useState("");
+  const [from_email, setFromEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [loading, setLoading] = useState(false);
+  const sendEmail = (e) => {
+    e.preventDefault();
+    setLoading(true);
+    emailjs
+      .send(
+        "service_apsgdlv",
+        "template_maroa3j",
+        {
+          to_name: "Ravikumar",
+          from_name: from_name,
+          from_email: from_email,
+          message: message,
+          reply_to: from_email,
+        },
+
+        "bBLoy3Y_EniTY0J-7"
+      )
+      .then(
+        () => {
+          console.log("SUCCESS!");
+        },
+        (error) => {
+          console.log("FAILED...", error);
+        }
+      );
+    setFromName("");
+    setFromEmail("");
+    setMessage("");
+    setLoading(false);
+  };
+  return (
+    <div className="w-full">
+      <form
+        onSubmit={sendEmail}
+        className="border-2 border-gray-700 flex flex-col gap-4 p-3 rounded-lg backdrop-blur-md ">
+        <div className="flex flex-col gap-2 ">
+          <label htmlFor="name" className="font-semibold ">
+            Name
+          </label>
+          <input
+            className="bg-gray-700 bg-opacity-50 rounded-sm h-12 pl-2"
+            type="text"
+            name="from_name"
+            id="name"
+            placeholder="Your name"
+            value={from_name}
+            onChange={(e) => setFromName(e.target.value)}
+          />
+        </div>
+        <div className="flex flex-col gap-2">
+          <label htmlFor="email" className="font-semibold">
+            E-mail
+          </label>
+          <input
+            className="bg-gray-700  bg-opacity-50 rounded-sm h-12 pl-2"
+            type="email"
+            name="from_email"
+            id="email"
+            placeholder="me@email.com"
+            value={from_email}
+            onChange={(e) => setFromEmail(e.target.value)}
+          />
+        </div>
+        <div className="flex flex-col gap-2">
+          <label className="font-semibold" htmlFor="message">
+            Message
+          </label>
+          <textarea
+            className="bg-gray-700  bg-opacity-50 rounded-sm min-h-20 max-h-20 pl-2 py-2 "
+            name="message"
+            id="message"
+            placeholder="send your thoughts "
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}></textarea>
+        </div>
+        <button
+          type="submit"
+          className={`  mx-auto w-3/4 md:w-2/4 cursor-pointer ${
+            loading ? "bg-green-500" : "bg-blue-500"
+          } text-white  py-2 rounded-md`}>
+          Send
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default ContactForm;
---
+++ src/Components/Project.jsx
@@ -0,0 +1,107 @@
+import project1 from "../assets/project1.png";
+import project2 from "../assets/project2.png";
+
+const Project = () => {
+  return (
+    <div id="project" className="min-h-screen w-full pt-24 ">
+      <span className="text-blue-400 text-3xl md:text-5xl font-bold block text-center mb-8">
+        My Projects
+      </span>
+
+      <div className="h-full w-full flex flex-col gap-12 px-6">
+        {/* Project 1 */}
+        <div className="flex flex-col md:flex-row items-center gap-8">
+          <div className="md:w-1/2 border-2 border-gray-400 items-center p-4 flex flex-col gap-2 rounded-lg hover:shadow-lg transition-shadow">
+            <img
+              src={project1}
+              alt="Memory Game"
+              className="rounded-md py-2  "
+            />
+            <a
+              href="https://memory-game-eight-eosin.vercel.app/"
+              className="p-2 bg-indigo-700 w-fit lg:w-1/4 text-center rounded-lg"
+              target="_blank">
+              Memory Game
+            </a>
+          </div>
+          <div className="md:w-1/2 text-gray-300 flex flex-col gap-6">
+            <h1 className="text-2xl text-blue-400 font-semibold">
+              Memory Game
+            </h1>
+            <p className="text-xl text-justify">
+              This interactive memory game challenges players to match pairs of
+              cards with identical images. Built using React.js, it ensures a
+              smooth and responsive gaming experience. CSS animations enhance
+              visual appeal, making it engaging and fun.
+            </p>
+
+            <hr className="border-gray-500" />
+            <div className="flex flex-col gap-2 items-center text-xl ">
+              <span className="text-gray-400 self-start">TECH STACK:</span>
+              <span className="text-white self-start">
+                React.js, JavaScript, CSS
+              </span>
+            </div>
+            <hr className="border-gray-500" />
+          </div>
+        </div>
+
+        {/* Divider */}
+        <div className="w-full h-[2px] bg-gray-700"></div>
+
+        {/* Project 2 */}
+        <div className="flex flex-col md:flex-row items-center gap-8">
+          <div className="md:w-1/2 border-2 border-gray-400 items-center p-4 flex flex-col gap-2 rounded-lg hover:shadow-lg transition-shadow">
+            <img src={project2} alt="Alcognerd" className="rounded-md py-2" />
+            <a
+              href="https://alcognerd.site/"
+              className="p-2 bg-indigo-700 w-fit lg:w-1/4 text-center rounded-lg"
+              target="_blank">
+              Alcognerd
+            </a>
+          </div>
+          <div className="md:w-1/2 text-gray-300 flex flex-col gap-6">
+            <p className="text-xl text-justify">
+              Alcognerd is a powerful event management and planning application
+              designed to streamline event coordination with real-time features.
+              The platform enables seamless communication between attendees and
+              organizers through an integrated real-time chat, fostering
+              engagement and collaboration. With smart attendance tracking,
+              powered by Redis, event hosts can monitor check-ins with accuracy
+              and efficiency. Secure Razorpay payment integration allows users
+              to register and pay for events effortlessly. The intuitive
+              scheduling system ensures that event creation, updates, and
+              management are smooth and hassle-free.
+            </p>
+            <hr className="border-gray-500" />
+            <div className="flex flex-col gap-2 items-center">
+              <span className="text-gray-400 self-start">TECH STACK:</span>
+              <span className="text-white flex text-xl flex-col gap-2 self-start">
+                <span>
+                  <strong>Frontend :</strong> React.js , JavaScript, Tailwind
+                  CSS,React Query
+                </span>
+                <span>
+                  <strong>Backend:</strong> Node.js ,Express.js, MongoDB, Redis
+                </span>
+                <span>
+                  <strong> Payments: </strong> Razorpay integration
+                </span>
+                <span>
+                  <strong> Real-Time Features: </strong>
+                  WebSockets for chat and Redis for attendance tracking
+                </span>
+                <span>
+                  <strong>Deployment: </strong>
+                  AWS EC2, Vercel
+                </span>
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Project;
